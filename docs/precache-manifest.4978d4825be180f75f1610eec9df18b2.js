37612185f9c4f1bf7ecc139bfd021de4ed2e6b1e
Amy Y <ctrlalternativedelete@gmail.com>
2021-02-27 13:16:47 +1100

    update dist

---
+++ docs/precache-manifest.4978d4825be180f75f1610eec9df18b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870d5d3eaef1ee178889",
+    "url": "/css/app.fbb4b0e7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "98b3f607b7e56d27d98b583b56a01c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "669765f1b1089bb686a3",
+    "url": "/js/about.2182e579.js"
+  },
+  {
+    "revision": "870d5d3eaef1ee178889",
+    "url": "/js/app.c28b925b.js"
+  },
+  {
+    "revision": "57c9cba79a6796f96b23",
+    "url": "/js/chunk-vendors.ca445ebb.js"
+  },
+  {
+    "revision": "b1b17ba3b865e4551ebd9a2367ae7b7d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
